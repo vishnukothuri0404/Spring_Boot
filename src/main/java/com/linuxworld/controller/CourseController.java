7fa751ba661ca6a534ef97becbc16a28603e3aa1
“tejchaitanya” <vishnukrishna04@yahoo.com>
2024-09-28 00:17:55 -0400

    add

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello LW , Final Tc";
+        return "Hello Vishnu , Final Tc";
     }
 
 
